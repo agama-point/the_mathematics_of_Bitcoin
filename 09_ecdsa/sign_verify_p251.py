373f5dbd00938a9b249a377fa4ac8c3ff1570d96
Honza S. Copak <bonum.comunae@volny.cz>
2026-02-11 12:31:51 +0100

    fix bob:h(msg)

--- 09_ecdsa/sign_verify_p251.py
+++ 09_ecdsa/sign_verify_p251.py
@@ -31,7 +31,8 @@ def run_demo(debug_mode=False):
     is_valid = ess251.verify(alice_pub, h, signature, debug=debug_mode)
     
     print(f"Final Result: {'Valid ✅' if is_valid else 'Invalid ❌'}")
-
+    print("-"*30)
+    print()
     # ---------- Bob -----------------
     bob_priv = 222
     bob_pub = ess251.scalar_mult(bob_priv, ess251.G_POINT)
@@ -40,7 +41,7 @@ def run_demo(debug_mode=False):
     h = simple_hash(msg)
     if debug_mode:
        print("[DEBUG_1_SETUP]", bob_priv, bob_pub)
-       print("[DEBUG_2_MESSAGE_HASH]", msg, hex(h),"? js: 0x06eab2")
+       print("[DEBUG_2_MESSAGE_HASH]", msg, hex(h),"? js: 0x20a8df")
 
     signature = ess251.sign(bob_priv, h, debug=debug_mode)
     is_valid = ess251.verify(bob_pub, h, signature, debug=debug_mode)
@@ -51,17 +52,13 @@ if __name__ == "__main__":
     print("=== AGAMA ECC SYSTEM INTERFACE ===")
     
     # Quiet run
-    run_demo(debug_mode=False)
+    # run_demo(debug_mode=False)
     
     # Verbose run
     run_demo(debug_mode=True)
 
 """
 === AGAMA ECC SYSTEM INTERFACE ===
-
---- RUNNING DEMO (Debug: False) ---
-Final Result: Valid ✅
-Final Result: Valid ✅
 
 --- RUNNING DEMO (Debug: True) ---
 [DEBUG_1_SETUP] 111 (131, 193)
@@ -77,21 +74,13 @@ Final Result: Valid ✅
 [DEBUG-VERIFY] L (s*G): (155, 16)
 [DEBUG-VERIFY] P (R + e*PubKey): (155, 16)
 Final Result: Valid ✅
+------------------------------
+
 [DEBUG_1_SETUP] 222 (232, 117)
-[DEBUG_2_MESSAGE_HASH] BobTest567 0x20a8df ? js: 0x06eab2
+[DEBUG_2_MESSAGE_HASH] BobTest567 0x20a8df ? js: 0x20a8df
 
 [DEBUG-SIGN] Starting signing process...
 [DEBUG-SIGN] Nonce k: 62
-[DEBUG-SIGN] R-Point (k*G): (226, 107)
-[DEBUG-SIGN] Signature components: r=226, s=188
-
-[DEBUG-VERIFY] Starting verification process...
-[DEBUG-SIGN] R-Point (k*G): (226, 107)
-[DEBUG-SIGN] Signature components: r=226, s=188
-
-[DEBUG-SIGN] R-Point (k*G): (226, 107)
-[DEBUG-SIGN] Signature components: r=226, s=188
-[DEBUG-SIGN] R-Point (k*G): (226, 107)
 [DEBUG-SIGN] R-Point (k*G): (226, 107)
 [DEBUG-SIGN] Signature components: r=226, s=188
 
